06d63ddb3a0dbf3f37ec8cf976ef3eb0594e9b95
jjfrl <jlinley28@gmail.com>
2023-04-23 12:09:56 +0100

    Removed closing of dialog box when add is pressed

--- random_generator_app/lib/decision_page.dart
+++ random_generator_app/lib/decision_page.dart
@@ -25,7 +25,7 @@ class _DecisionPageState extends State<DecisionPage> {
       color: Color.fromARGB(255, 76, 99, 109),
     ),
   );
-  String decisionTxt = "...";
+  // String decisionTxt = "...";
 
   final _controller = TextEditingController();
   TextEditingController questionText = TextEditingController();
@@ -44,7 +44,7 @@ class _DecisionPageState extends State<DecisionPage> {
       optionsList.add([_controller.text]);
       _controller.clear();
     });
-    Navigator.of(context).pop();
+    // Navigator.of(context).pop();
   }
 
   //method to create a new option
@@ -109,6 +109,7 @@ class _DecisionPageState extends State<DecisionPage> {
     return dText = dText
         .animate(
           onPlay: (controller) => controller.loop(count: 3),
+          onComplete: (controller) => controller.dispose(),
         )
         .fade(duration: 1000.ms)
         .tint(color: midnightColour)
@@ -122,156 +123,159 @@ class _DecisionPageState extends State<DecisionPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          'Random Generator',
-          style: TextStyle(
+    return GestureDetector(
+      onTap: () => FocusManager.instance.primaryFocus?.unfocus(),
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text(
+            'Random Generator',
+            style: TextStyle(
+              color: Colors.black,
+            ),
+          ),
+          centerTitle: true,
+          actions: [
+            IconButton(
+              onPressed: refreshPage,
+              icon: const Icon(Icons.refresh),
+              color: Colors.black,
+            )
+          ],
+          backgroundColor: oceanBlueColour,
+          leading: IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.dark_mode),
             color: Colors.black,
           ),
-        ),
-        centerTitle: true,
-        actions: [
-          IconButton(
-            onPressed: refreshPage,
-            icon: const Icon(Icons.refresh),
-            color: Colors.black,
-          )
-        ],
-        backgroundColor: oceanBlueColour,
-        leading: IconButton(
-          onPressed: () {},
-          icon: const Icon(Icons.dark_mode),
-          color: Colors.black,
-        ),
-        shape: const RoundedRectangleBorder(
-          borderRadius: BorderRadius.only(
-            bottomLeft: Radius.circular(10),
-            bottomRight: Radius.circular(10),
+          shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.only(
+              bottomLeft: Radius.circular(10),
+              bottomRight: Radius.circular(10),
+            ),
           ),
         ),
-      ),
-      body: Column(
-        children: [
-          const SizedBox(
-            height: 10,
-          ),
-          Container(
-            margin: const EdgeInsets.all(5),
-            padding: const EdgeInsets.all(5),
-            child: TextField(
-              style: const TextStyle(fontSize: 20),
-              controller: questionText,
-              decoration: InputDecoration(
-                border: const OutlineInputBorder(),
-                labelText: 'Enter your question',
-                labelStyle: TextStyle(
-                    color: midnightColour,
-                    fontStyle: FontStyle.italic,
-                    fontSize: 16),
-                suffixIcon: IconButton(
-                  onPressed: () {
-                    questionText.clear();
-                  },
-                  icon: const Icon(Icons.clear),
+        body: Column(
+          children: [
+            const SizedBox(
+              height: 10,
+            ),
+            Container(
+              margin: const EdgeInsets.all(5),
+              padding: const EdgeInsets.all(5),
+              child: TextField(
+                style: const TextStyle(fontSize: 20),
+                controller: questionText,
+                decoration: InputDecoration(
+                  border: const OutlineInputBorder(), //change border properties
+                  labelText: 'Enter your question',
+                  labelStyle: TextStyle(
+                      color: midnightColour,
+                      fontStyle: FontStyle.italic,
+                      fontSize: 16),
+                  suffixIcon: IconButton(
+                    onPressed: () {
+                      questionText.clear();
+                    },
+                    icon: const Icon(Icons.clear),
+                  ),
                 ),
               ),
             ),
-          ),
-          ConstrainedBox(
-            constraints: const BoxConstraints(minWidth: 100, minHeight: 40),
-            child: ElevatedButton.icon(
-              style: ElevatedButton.styleFrom(
-                padding: const EdgeInsets.all(10),
-                backgroundColor: oceanBlueColour,
-                shadowColor: midnightColour,
-                elevation: 5,
-              ),
-              label: const Text(
-                'Enter option',
-                style: TextStyle(
-                  fontSize: 20,
+            ConstrainedBox(
+              constraints: const BoxConstraints(minWidth: 100, minHeight: 40),
+              child: ElevatedButton.icon(
+                style: ElevatedButton.styleFrom(
+                  padding: const EdgeInsets.all(10),
+                  backgroundColor: oceanBlueColour,
+                  shadowColor: midnightColour,
+                  elevation: 5,
+                ),
+                label: const Text(
+                  'Enter option',
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.black,
+                  ),
+                ),
+                icon: const Icon(
+                  Icons.add,
                   color: Colors.black,
                 ),
-              ),
-              icon: const Icon(
-                Icons.add,
-                color: Colors.black,
-              ),
-              onPressed: () {
-                createNewOption();
-              },
-            ),
-          ),
-          const SizedBox(
-            height: 10,
-          ),
-          const Divider(
-            thickness: 1,
-            color: Colors.blueGrey,
-            height: 5,
-          ),
-          Expanded(
-            child: Container(
-              color: const Color.fromARGB(125, 199, 218, 201),
-              child: ListView.builder(
-                itemCount: optionsList.length,
-                itemBuilder: (BuildContext context, int index) {
-                  return DecisionItem(
-                    optionText: optionsList[index][0],
-                    deleteFunction: (context) => deleteOption(index),
-                  );
+                onPressed: () {
+                  createNewOption();
                 },
               ),
             ),
-          ),
-          const Divider(
-            thickness: 1,
-            color: Colors.blueGrey,
-            height: 5,
-          ),
-          const SizedBox(
-            height: 10,
-          ),
-          ConstrainedBox(
-            constraints: const BoxConstraints(
-                minWidth: 200, minHeight: 40), //width: 200, height: 40
-            child: ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                padding: const EdgeInsets.all(10),
-                backgroundColor: oceanBlueColour,
-                shadowColor: midnightColour,
-                elevation: 5,
-              ),
-              child: const Text(
-                'Generate',
-                style: TextStyle(
-                  fontSize: 20,
-                  color: Colors.black,
+            const SizedBox(
+              height: 10,
+            ),
+            const Divider(
+              thickness: 1,
+              color: Colors.blueGrey,
+              height: 5,
+            ),
+            Expanded(
+              child: Container(
+                color: const Color.fromARGB(125, 199, 218, 201),
+                child: ListView.builder(
+                  itemCount: optionsList.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    return DecisionItem(
+                      optionText: optionsList[index][0],
+                      deleteFunction: (context) => deleteOption(index),
+                    );
+                  },
                 ),
               ),
-              onPressed: () {
-                randomDecision();
-              },
             ),
-          ),
-          const SizedBox(
-            height: 5,
-          ),
-          Container(
-            decoration: BoxDecoration(
-              borderRadius: const BorderRadius.all(
-                Radius.circular(15),
+            const Divider(
+              thickness: 1,
+              color: Colors.blueGrey,
+              height: 5,
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            ConstrainedBox(
+              constraints: const BoxConstraints(
+                  minWidth: 200, minHeight: 40), //width: 200, height: 40
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  padding: const EdgeInsets.all(10),
+                  backgroundColor: oceanBlueColour,
+                  shadowColor: midnightColour,
+                  elevation: 5,
+                ),
+                child: const Text(
+                  'Generate',
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.black,
+                  ),
+                ),
+                onPressed: () {
+                  randomDecision();
+                },
               ),
-              border: Border.all(color: burntOrangeColour, width: 2),
-              color: Colors.transparent,
             ),
-            height: 75,
-            alignment: Alignment.center,
-            margin: const EdgeInsets.all(5),
-            child: dText,
-          ),
-        ],
+            const SizedBox(
+              height: 5,
+            ),
+            Container(
+              decoration: BoxDecoration(
+                borderRadius: const BorderRadius.all(
+                  Radius.circular(15),
+                ),
+                border: Border.all(color: burntOrangeColour, width: 2),
+                color: Colors.transparent,
+              ),
+              height: 75,
+              alignment: Alignment.center,
+              margin: const EdgeInsets.all(5),
+              child: dText,
+            ),
+          ],
+        ),
       ),
     );
   }
